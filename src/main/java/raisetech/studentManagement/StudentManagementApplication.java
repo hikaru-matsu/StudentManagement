d8f63341834ec596ce23c727bb062cd94810a129
Hikaru <mhikarum.080@gmail.com>
2025-03-09 14:24:30 +0900

    Get&Post

--- src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,19 +1,31 @@
 package raisetech.studentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Hikaru";
+	private int age = 25;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-@GetMapping("/calc")
-	public static int add(int x, int y) {
-		return x + y;
+@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age;
+	}
+
+	@PostMapping("/studentInfo")
+		public void setStudentInfo(String name, int age){
+		this.name = name;
+		this.age = age;
 	}
 }
