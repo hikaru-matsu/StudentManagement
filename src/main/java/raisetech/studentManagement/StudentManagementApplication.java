68e11cab27e299b945d925186f7004dce0678752
Hikaru <mhikarum.080@gmail.com>
2025-03-13 16:18:51 +0900

    map

--- src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,31 +1,48 @@
 package raisetech.studentManagement;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	private String name = "Hikaru";
-	private int age = 25;
+	@Autowired
+	private StudentRepository repository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		return name + " " + age;
+	@GetMapping("/studentInfo")
+		public String getStudent(@RequestParam String name) {
+			Student student = repository.searchByName(name);
+			return student.getName() + " " + student.getAge() + "歳";
+		}
+
+	@GetMapping("/students")
+	public Map<String, Integer> getAllStudents() {
+		List<Student> students = repository.getAllStudents();
+		Map<String, Integer> studentMap = new HashMap<>();
+
+		for (Student student : students) {
+			studentMap.put(student.getName(), student.getAge());
+		}
+		return studentMap;
+
 	}
 
+
 	@PostMapping("/studentInfo")
-		public void setStudentInfo(String name, int age){
-		this.name = name;
-		this.age = age;
+		void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 }
---
+++ src/main/java/raisetech/studentManagement/StudentRepository.java
@@ -0,0 +1,20 @@
+package raisetech.studentManagement;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student")
+  List<Student> getAllStudents();
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+  @Insert("INSERT student values(#{name}, #{age})")
+  void registerStudent(String name, int age);
+}
