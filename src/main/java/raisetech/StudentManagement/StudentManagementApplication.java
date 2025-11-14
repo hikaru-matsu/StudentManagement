8a62e3490e33e853e2aae6c2d0c3698c5ab30bb5
Hikaru <mhikarum.080@gmail.com>
2025-11-14 09:34:10 +0900

    create branch

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -14,8 +14,8 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello";
+	@GetMapping("/calc")
+	public int calc() {
+		return 2 + 2;
 	}
 }
