2720e8c828769976295c47e4c3b99a41e350508a
Hikaru <mhikarum.080@gmail.com>
2025-11-25 09:07:50 +0900

    Rest化

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,22 +1,19 @@
 package raisetech.StudentManagement.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
   private StudentService service;
   private StudentConverter studentConveter;
@@ -28,46 +25,21 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public String getStudentList(Model model) {
+  public List<StudentDetail> getStudentList() {
     List<Student>studentList = service.searchStudentList();
     List<StudentCourse>studentCourseList = service.searchStudentCourseList();
-
-    model.addAttribute("studentList", studentConveter.convertStudentDetail(studentList, studentCourseList));
-    return "studentList";
-   }
-
-   @GetMapping("/newStudent")
-   public String newStudent(Model model) {
-    StudentDetail studentDetail = new StudentDetail();
-    StudentCourse studentCourse = new StudentCourse();
-    List<StudentCourse>studentCourseList = Arrays.asList(studentCourse);
-    studentDetail.setStudentCourse(studentCourseList);
-    model.addAttribute("studentDetail", studentDetail);
-    return "registerStudent";
+    return studentConveter.convertStudentDetail(studentList, studentCourseList);
    }
 
    @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if(result.hasErrors()) {
-      return "registerStudent";
-    }
+  public ResponseEntity<String> registerStudent(@RequestBody StudentDetail studentDetail) {
     service.registerStudent(studentDetail);
-    return "redirect:/studentList";
-   }
-
-   @GetMapping("/studentList/{id}")
-  public String getStudent(@PathVariable("id") Integer id, Model model) {
-    StudentDetail studentDetail = service.studentDetailFindById(id);
-    model.addAttribute(studentDetail);
-    return "updateStudent";
+    return ResponseEntity.ok("登録処理が成功しました！");
    }
 
    @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if(result.hasErrors()) {
-      return "updateStudent";
-    }
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
-    return "redirect:/studentList";
+    return ResponseEntity.ok("更新処理が成功しました！");
    }
 }
