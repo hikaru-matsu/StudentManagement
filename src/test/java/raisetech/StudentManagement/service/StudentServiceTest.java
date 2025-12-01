d64312e43b80ba3a89876db0037e9dccccf41482
Hikaru <mhikarum.080@gmail.com>
2025-12-02 00:01:33 +0900

    serviceno

---
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -0,0 +1,94 @@
+package raisetech.StudentManagement.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.repository.StudentRepository;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+  @Mock
+  private StudentRepository repository;
+
+  @Mock
+  private StudentConverter converter;
+
+  private StudentService sut;
+  private Student student;
+  private StudentCourse studentCourse;
+  private List<StudentCourse>studentCourseList;
+
+  @BeforeEach
+  void before() {
+    sut = new StudentService(repository, converter);
+    student = new Student();
+    studentCourse = new StudentCourse();
+    studentCourseList = new ArrayList<>();
+  }
+
+  @Test
+  void 受講生一覧検索機能＿リポジトリとコンバーターの処理が適切に呼び出せていること() {
+    List<Student>studentList = new ArrayList<>();
+
+    when(repository.searchStudent()).thenReturn(studentList);
+    when(repository.searchStudentCourse()).thenReturn(studentCourseList);
+
+    sut.searchStudentDetail();
+
+    Mockito.verify(repository, times(1)).searchStudent();
+    Mockito.verify(repository, times(1)).searchStudentCourse();
+    Mockito.verify(converter, times(1)).convertStudentDetail(studentList, studentCourseList);
+  }
+
+  @Test
+  void 受講生単一検索機能＿リポジトリの処理が適切に呼び出せていること() {
+    Integer id = 99;
+    student.setId(id);
+    studentCourse.setStudentId(id);
+    studentCourseList.add(studentCourse);
+
+    when(repository.studentFindById(id)).thenReturn(student);
+    when(repository.studentCoursesFindById(id)).thenReturn(studentCourseList);
+    sut.studentDetailFindById(id);
+
+    Mockito.verify(repository, times(1)).studentFindById(id);
+    Mockito.verify(repository, times(1)).studentCoursesFindById(id);
+  }
+
+  @Test
+  void 受講生登録機能＿リポジトリの処理が適切に呼びされていること() {
+    studentCourseList.add(studentCourse);
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentCourse(studentCourseList);
+
+    sut.registerStudent(studentDetail);
+
+    Mockito.verify(repository, times(1)).registerStudent(student);
+    Mockito.verify(repository, times(1)).registerStudentCourse(studentCourse);
+  }
+
+  @Test
+  void 受講生更新処理機能＿リポジトリの処理が適切に呼び出されていること() {
+    studentCourseList.add(studentCourse);
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+    sut.updateStudent(studentDetail);
+
+    Mockito.verify(repository, times(1)).updateStudent(student);
+    Mockito.verify(repository, times(1)).updateStudentCourse(studentCourse);
+  }
+
+}
